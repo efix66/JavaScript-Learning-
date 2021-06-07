2f96003b0cd3381912b28fd64204d353885172d1
Efix <efix66@gmail.com>
2021-06-07 19:17:12 +0300

    Am invatat despre metoda map

---
+++ Map/map.js
@@ -0,0 +1,22 @@
+//Map Method
+const people = [
+  {
+    name: "Felix",
+    age: 32,
+    position: "developer",
+  },
+  {
+    name: "Geanina",
+    age: 32,
+    position: "theacher",
+  },
+  {
+    name: "Horia",
+    age: 3,
+    position: "our kid",
+  },
+];
+
+// Map returns a new array
+// Does not change the size of the original array(unlike filter)
+// Uses the values from the original array whrn making a new one
