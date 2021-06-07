234d6f9f6286058188dd342431249bea83a6f516
Efix <efix66@gmail.com>
2021-06-07 19:42:16 +0300

    update map

--- Map/map.js
+++ Map/map.js
@@ -7,7 +7,7 @@ const people = [
   },
   {
     name: "Geanina",
-    age: 32,
+    age: 31,
     position: "theacher",
   },
   {
@@ -20,3 +20,18 @@ const people = [
 // Map returns a new array
 // Does not change the size of the original array(unlike filter)
 // Uses the values from the original array whrn making a new one
+
+const ages = people.map((person) => person.age * 2);
+console.log(ages);
+
+const newPeople = people.map((item) => {
+  return { name: item.name, age: item.age, position: item.position };
+});
+
+console.log(newPeople);
+
+const names = people.map((person) => `<h2>${person.name}</h2>`);
+
+const data = document.querySelector(".output-data");
+
+data.innerHTML = names.join("");
